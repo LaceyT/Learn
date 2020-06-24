0ccb9720b0c8af45e396f9c9fb8f005ec60bb39f
H202125 <Lacey.Tao@Honeywell.com>
2020-06-24 13:06:18 +0800

    User nextjs to render ui in server

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,5 +1,17 @@
 const express = require("express")
+const next = require("next")
 const app = express()
-app.listen(3000, ()=>{
-    console.log("server started on port 3000")
+
+const dev = process.env.NODE_ENV != 'production';
+const nextApp = next({dev});
+const nextHandler = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+    app.get('*', (req, res) => {
+        return nextHandler(req, res);
+    });
+
+    app.listen(3000, ()=>{
+        console.log("server started on port 3000")
+    });
 })
